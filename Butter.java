f25665744a45a596980afc07fbd65727db9b53e2
sampath <sampath.skt@gmail.com>
2015-01-11 13:05:14 +0530

    Butter done

--- Butter.java
+++ Butter.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 class Butter
 {
-     Scanner scn = new Scanner(System.in);
-
+     //     Scanner scn = new Scanner(System.in);
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
      public static void main(String[] args)
      {
 	  Butter b1 = new Butter();
@@ -12,47 +16,62 @@ class Butter
 
      void begin()
      {
-	  int testC = scn.nextInt();
-	  
-	  for(int i=0;i<testC;i++)
+	  try
 	  {
-	       int numBoxes = scn.nextInt();
-	       int value[] = new int[numBoxes];
-	       
-	       for(int j =0;j<numBoxes;j++)
-	       {
-		    value[j] = scn.nextInt();	    
-	       }
-	       qSort(value,0,value.length-1);
-	       int c,n,lb=1,ub=1;
+	       int testC = Integer.valueOf(br.readLine());
+	       //	  System.out.println("test cases: "+testC);
 
-	       for(int k = 0;k<(value.length-1);k++)
+	       for(int i=0;i<testC;i++)
 	       {
-		    c = value[k];
-		    n = value[k+1];
-		    
-		    if(c==n)
+		    int numBoxes = Integer.valueOf(br.readLine());
+		    //	       System.out.println("boxes in test case "+(i+1)+" "+numBoxes);
+		    int value[] = new int[numBoxes];
+		    //	       System.out.println("box values");
+
+		    for(int j =0;j<numBoxes;j++)
 		    {
-			 lb++;
+			 value[j] = Integer.valueOf(br.readLine());	    
+			 //		    System.out.println(value[j]);
 		    }
-		    else
+
+		    Arrays.sort(value);
+		    //qSort(value,0,value.length-1);
+		    //	       int c,n;
+		    int lb=1,ub=1;
+
+		    for(int k = 0;k<(value.length-1);k++)
 		    {
-			 if(ub<lb)
+			 //		    c = value[k];
+			 //		    n = value[k+1];
+			 //		    System.out.println("c and n "+c+" "+n);  
+			 if(value[k]==value[k+1])
 			 {
-			      ub = lb;
+			      lb++;
 			 }
-			 lb=1;
+			 else
+			 {
+			      if(ub<lb)
+			      {
+				   ub = lb;
+			      }
+			      lb=1;
+			 }
 		    }
+		    if(ub<lb)
+			 ub=lb;
+		    System.out.println(ub);
 	       }
-	       System.out.println(ub);
+	  }
+	  catch(IOException e)
+	  {
 	  }
 
      }
 
-     void qSort(int[] value,int begin,int end)
+     void qSort(long[] value,int begin,int end)
      {
 
-	  if(begin!=end)
+	  if(begin<end)
 	  {
 	       int pivInd = partition(value,begin,end);
 	       qSort(value,begin,pivInd-1);
@@ -60,12 +79,12 @@ class Butter
 	  }
      }
 
-     int partition(int[] value,int begin,int end)
+     int partition(long[] value,int begin,int end)
      {
-	  int piv = value[end-1];
+	  long piv = value[end-1];
 	  int lessEle = begin-1;
-	  int temp;
-	  
+	  long temp;
+
 	  for(int i = begin;i<=end;i++)
 	  {
 	       if(value[i]<=piv)
