ae3e7a0775ff24ddb8c838f8af4ceaf97ee5e0c4
ssurinen <sampath.surineni@oracle.com>
2017-03-07 17:55:54 +0530

    added parity

---
+++ parity.py
@@ -0,0 +1,17 @@
+http://www.practice.geeksforgeeks.org/editorial.php?pid=395
+test_cases = int(input().strip())
+# print("test_cases " + str(test_cases))
+for i in range(test_cases):
+    numb = int(input().strip())
+    parity_count = 0
+    # could have used boolean and not instead of int
+    while(numb > 0):
+        if numb % 2 == 1:
+            parity_count += 1
+
+        numb = int(numb / 2)
+
+    if parity_count % 2 == 0:
+        print ('even')
+    else:
+        print ('odd')
