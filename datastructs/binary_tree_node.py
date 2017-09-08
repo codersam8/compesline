57df419337eaa581f3e1b60dc203a0c61c0f5729
sampath surineni <sampath.surineni@oracle.com>
2017-09-08 18:18:49 +0530

    added binary tree node

---
+++ datastructs/binary_tree_node.py
@@ -0,0 +1,5 @@
+class BinaryTreeNode:
+    def __init__(self, ele=None, lc=None, rc=None):
+        self.ele = ele
+        self.lc = lc
+        self.rc = rc
--- datastructs/linked_binary_tree.py
+++ datastructs/linked_binary_tree.py
@@ -2,5 +2,13 @@ class linked_binary_tree:
     def __init__(self):
         self.root = None
 
+    def make_tree(self, root, left, right):
+        self.root = None
+
+
 if __name__ == '__main__':
-    lbt = linked_binary_tree()
+    a = linked_binary_tree()
+    w = linked_binary_tree()
+    x = linked_binary_tree()
+    y = linked_binary_tree()
+    z = linked_binary_tree()
