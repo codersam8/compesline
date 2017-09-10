1baeb87d970c21106c14672f8cdaac34ee59c0c1
Sampath Surineni <sampath.surineni@gmail.com>
2017-09-10 21:47:30 +0530

    latest changes

--- datastructs/linked_binary_tree.py
+++ datastructs/linked_binary_tree.py
@@ -1,9 +1,20 @@
+from binary_tree_node import BinaryTreeNode
+
+
 class linked_binary_tree:
     def __init__(self):
         self.root = None
 
     def make_tree(self, root, left, right):
-        self.root = None
+        self.root = BinaryTreeNode(root,
+                                   left.root,
+                                   right.root)
+
+    def the_output(bi_tree_node):
+        print(bi_tree_node.ele + ' ')
+
+    def pre_order_output():
+        pre_order(the_output)
 
 
 if __name__ == '__main__':
@@ -12,3 +23,8 @@ if __name__ == '__main__':
     x = linked_binary_tree()
     y = linked_binary_tree()
     z = linked_binary_tree()
+    w.make_tree(1, a, a)
+    x.make_tree(2, a, a)
+    y.make_tree(3, w, x)
+    z.make_tree(4, x, a)
+    print("Preorder sequence is ")
