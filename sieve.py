9f563b278f0b3ac3ee5ef5d0425c8e6fb3ad7d4a
sampath surineni <sampath.surineni@oracle.com>
2017-10-10 18:40:31 +0530

    added new program

---
+++ sieve.py
@@ -0,0 +1,18 @@
+# practice.geeksforgeeks.org/problems/sieve-of-eratosthenes/0/
+test_cases = int(input().strip())
+lim_list = []
+for a_tc in range(test_cases):
+    lim_list.append(int(input().strip()))
+biggest = max(lim_list)
+sieve_list = [True] * (biggest + 1)
+
+for val in range(2, biggest + 1):
+    if sieve_list[val]:
+        for val2 in range((2*val), biggest + 1, val):
+            sieve_list[val2] = False
+
+for lim in lim_list:
+    for val in range(2, lim + 1):
+        if sieve_list[val]:
+            print(val, end=' ')
+    print()
