96f6ce39a16c5c7af7c3d360b5963cec4768fa7d
Sampath Surineni <sampath.surineni@gmail.com>
2017-10-15 13:10:52 +0530

    added one more program

---
+++ pow_2.py
@@ -0,0 +1,10 @@
+# http://practice.geeksforgeeks.org/problems/power-of-2/0
+test_cases = int(input().strip())
+
+for a_tc in range(test_cases):
+    a_num = int(input().strip())
+    a_num = a_num if not a_num else (a_num & (a_num - 1))
+    if not a_num:
+        print('YES')
+    else:
+        print('NO')
